0096cf0a64a11a15487508880c7578e92a7345d2
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2021-02-06 00:39:41 +0200

    done

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,5 +1,6 @@
-import React, {useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import EmailIcon from '@material-ui/icons/Email';
 
 import db from '../firebase'
 import '../css/Account.css'
@@ -7,17 +8,17 @@ import { useStateValue } from "../components/StateProvider";
 
 export default function Account() {
     const { accountId } = useParams();
-    const [{user}, dispatch ] = useStateValue();
-    const [ userDetails , setUserDetails ] = useState("");
+    const [{ user }, dispatch] = useStateValue();
+    const [userDetails, setUserDetails] = useState("");
 
 
-    useEffect( () => {
+    useEffect(() => {
         const unsubscribe = db.collection('users').doc(accountId).onSnapshot((snapshot) => setUserDetails(snapshot.data()));
-      
+
         return () => {
             unsubscribe();
         }
-    },[]);
+    }, []);
 
     // const formHandler = () =>{
     //     db.collection('users').doc(user.uid).add({
@@ -27,12 +28,12 @@ export default function Account() {
     //     })
     // }
 
-    return accountId?(
+    return accountId ? (
         <div className='account'>
             <div className='account__header'>
                 <img src={userDetails?.photoURL} alt='profile_picture' />
                 <h1>{userDetails.displayName}</h1>
-                
+
             </div>
             <div className='account__info'>
                 <h2>Account information: </h2>
@@ -41,19 +42,25 @@ export default function Account() {
             </div>
 
         </div>
-    ):(
-        <div className='account'>
-        <div className='account__header'>
-            <img src={user?.photoURL} alt='profile_picture' />
-            <h1>{user.displayName}</h1>
-            
-        </div>
-        <div className='account__info'>
-            <h2>Account information: </h2>
-            <p><b>Email: </b>{user.email}</p>
-            <p><b>Last Login: </b>{user.metadata.lastSignInTime}</p>
-        </div>
+    ) : (
+            <div className='account'>
+                <div className='account__header'>
+                    <div className= 'imgContainer'>
+                        <img src={user?.photoURL} alt='profile_picture' />
+                    </div>
 
-    </div>
-    )
+                    <h1></h1>
+
+                </div>
+                <div className='account__info'>
+                    <br/>
+                    <h1>{user.displayName}</h1>
+                    
+                     <br/>   
+                    <p> <b><EmailIcon style={{ fontSize: 200 }}/> : </b>{user.email}</p>
+                    <p><b>Last Login: </b>{user.metadata.lastSignInTime}</p>
+                </div>
+
+            </div>
+        )
 }
