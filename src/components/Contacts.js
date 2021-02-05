1ad005053668b385627de4b83aaba127d4ee4789
sallytareq <sally.ammous@gmail.com>
2021-02-05 17:43:48 +0200

    chat avatar and display name links

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,10 +4,12 @@ import { useStateValue } from "../components/StateProvider";
 import ContactsCard from '../components/ContactsCard'
 import db from '../firebase'
 import { Link } from 'react-router-dom'
+import { Search } from "@material-ui/icons";
 
 export default function Contacts() {
     const [{user}, dispatch ] = useStateValue();
     const [ users , setUsers ] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect( () => {
         const unsubscribe = db.collection('users').onSnapshot(snapshot =>(
@@ -26,6 +28,13 @@ export default function Contacts() {
             <div className='contacts__header'>
                 <h1>Contacts</h1>
             </div>
+            <div className='contacts__search'>
+                <form>
+                    <input value={searchValue} onChange={(e)=>setSearchValue(e.target.value)} type='text' placeholder='Type an email' />
+                    <button type='submit' ><Search /></button>
+                </form>
+            </div>
+        
             <div className='contacts__cards'>
                 {/* <ContactsCard addNewContact/> */}
                 {users && users.map(userDetails =>(
