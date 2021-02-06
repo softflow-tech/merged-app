c39553dfc2252b055a407b59b2b7ac31aa998017
Sally Ammous <70090183+sallytareq@users.noreply.github.com>
2021-02-06 03:41:11 +0200

    config for vercel

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8bk8X1gP-IK1GUqzTfQBDboSrXW2ElBs",
-  authDomain: "test-clone-8f056.firebaseapp.com",
-  databaseURL: "https://test-clone-8f056-default-rtdb.firebaseio.com",
-  projectId: "test-clone-8f056",
-  storageBucket: "test-clone-8f056.appspot.com",
-  messagingSenderId: "369779514054",
-  appId: "1:369779514054:web:fa9bbc1459d27b97d3d5c7",
-  measurementId: "G-E1Y58N9E59"
+  apiKey: "AIzaSyAKG6eTd3um51srwjLSBxXJg90yD5WY1mU",
+  authDomain: "chat-app-test-vercel.firebaseapp.com",
+  databaseURL: "https://chat-app-test-vercel-default-rtdb.firebaseio.com",
+  projectId: "chat-app-test-vercel",
+  storageBucket: "chat-app-test-vercel.appspot.com",
+  messagingSenderId: "770570403324",
+  appId: "1:770570403324:web:e5e0a7b06f258687aa4679",
+  measurementId: "G-QGS1EZL7P2"
 };
 
 
