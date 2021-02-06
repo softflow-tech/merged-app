554f10df0966ee78a48e03a22b3ca36eafa4939e
Sally Ammous <70090183+sallytareq@users.noreply.github.com>
2021-02-06 03:25:45 +0200

    firebase confic for vercel

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC18yxvskPTNpcN-Tz4nFikJ5i9QtPuta4",
-  authDomain: "backup2-49c2f.firebaseapp.com",
-  projectId: "backup2-49c2f",
-  storageBucket: "backup2-49c2f.appspot.com",
-  messagingSenderId: "655035542798",
-  appId: "1:655035542798:web:7297998ae93e40e9673b5f",
-  measurementId: "G-ZY8F6NCSPN"
+  apiKey: "AIzaSyC8bk8X1gP-IK1GUqzTfQBDboSrXW2ElBs",
+  authDomain: "test-clone-8f056.firebaseapp.com",
+  databaseURL: "https://test-clone-8f056-default-rtdb.firebaseio.com",
+  projectId: "test-clone-8f056",
+  storageBucket: "test-clone-8f056.appspot.com",
+  messagingSenderId: "369779514054",
+  appId: "1:369779514054:web:fa9bbc1459d27b97d3d5c7",
+  measurementId: "G-E1Y58N9E59"
 };
 
 
